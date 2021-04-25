7c689ba5e6f9ae5c77edd9de434c44723bfeb9c5
Szabolcs <szabolcs.pinter@stud.uni-corvinus.hu>
2021-04-25 23:40:11 +0200

    9.hét

--- feladatok/HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ feladatok/HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -95,4 +95,16 @@ window.onload = () => {
         document.getElementById("válasz2").style.pointerEvents = 'none';
         document.getElementById("válasz3").style.pointerEvents = 'none';
     }
+    fetch('/questions/1')
+        .then(response => response.json())
+        .then(data => kérdésMegjelenítés(data)
+        );
+
+    function kérdésMegjelenítés(kérdés) {
+        console.log(kérdés);
+        document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+        document.getElementById("válasz1").innerText = kérdés.answer1
+        document.getElementById("válasz2").innerText = kérdés.answer2
+        document.getElementById("válasz3").innerText = kérdés.answer3
+        document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;  
 }
